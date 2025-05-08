29e8ddd51c4cf9eadbbc49864845c6c0c72fb881
VladyslavaDanylina <vladyslavadanylina@gmail.com>
2025-05-08 22:05:06 +0300

    spotify demo play

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,73 +1,71 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Track.css";
 
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-  }
+const Track = ({ track, onAdd, onRemove, isRemoval, onPlay }) => {
+  const { name, artist, album, albumImage, previewUrl } = track;
+  const audioRef = useRef(null);
+  const [volume, setVolume] = useState(0.5);
 
-  addTrack() {
-    this.props.onAdd(this.props.track);
-  }
+  const addTrack = () => onAdd && onAdd(track);
+  const removeTrack = () => onRemove && onRemove(track);
 
-  removeTrack() {
-    this.props.onRemove(this.props.track);
-  }
+  const handlePlay = () => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume;
+      audioRef.current.play();
+    }
+    onPlay && onPlay(track.uri); // optional external handler
+  };
 
-  renderAction() {
-    const { isRemoval } = this.props;
-    return (
-      <button
-        className="Track-action"
-        onClick={isRemoval ? this.removeTrack : this.addTrack}
-      >
-        {isRemoval ? "−" : "+"}
-      </button>
-    );
-  }
+  const handleVolumeChange = (e) => {
+    const newVolume = parseFloat(e.target.value);
+    setVolume(newVolume);
+    if (audioRef.current) {
+      audioRef.current.volume = newVolume;
+    }
+  };
 
-  render() {
-    const { name, artist, album, albumImage, previewUrl, uri } = this.props.track;
+  const renderAction = () => (
+    <button className="Track-action" onClick={isRemoval ? removeTrack : addTrack}>
+      {isRemoval ? "-" : "+"}
+    </button>
+  );
 
-    return (
-      <div className="Track">
-        <div className="Track-left">
-          {this.props.onPlay && uri && (
-            <button
-              onClick={() => this.props.onPlay(uri)}
-              className="Track-play-btn"
-            >
-              ▶
-            </button>
-          )}
-
-          {albumImage && (
-            <img
-              src={albumImage}
-              alt={`${name} album cover`}
-              className="Track-album-art"
-            />
-          )}
-        </div>
-
-        <div className="Track-information">
-          <h3>{name}</h3>
-          <p>{artist} | {album}</p>
-
-          {previewUrl && (
-            <audio controls className="Track-audio">
-              <source src={previewUrl} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-          )}
-        </div>
-
-        {this.renderAction()}
+  return (
+    <div className="Track">
+      <div className="Track-left">
+        {previewUrl && (
+          <button className="Track-play-button" onClick={handlePlay}>▶</button>
+        )}
+        {albumImage && (
+          <img
+            src={albumImage}
+            alt={`${name} album art`}
+            className="Track-album-art"
+          />
+        )}
+        {previewUrl && (
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={handleVolumeChange}
+            className="Track-volume"
+          />
+        )}
       </div>
-    );
-  }
-}
+      <div className="Track-information">
+        <h3>{name}</h3>
+        <p>{artist} | {album}</p>
+        {previewUrl && (
+          <audio ref={audioRef} src={previewUrl} preload="auto" />
+        )}
+      </div>
+      {renderAction()}
+    </div>
+  );
+};
 
 export default Track;
