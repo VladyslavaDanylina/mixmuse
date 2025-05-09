7faee241e2eeba55c025b90460fa6b7f8763f5c0
VladyslavaDanylina <vladyslavadanylina@gmail.com>
2025-05-09 19:08:46 +0300

    final v5

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,6 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import PlaylistList from "../PlaylistList/PlaylistList";
-import SpotifyPlayer from "../SpotifyPlayer/SpotifyPlayer";
 import Spotify from "../../util/Spotify";
 
 class App extends React.Component {
@@ -12,28 +11,21 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      accessToken: null,
       searchResults: [],
       playlistName: "New Playlist Name",
       playlistTracks: [],
-      playlists: [],
-      currentlyPlayingUri: null, // ⬅️ new state for playback
+      playlists: [], // array of objects to store each playlist's playlistId and playlistName
     };
-
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
     this.search = this.search.bind(this);
     this.getUserPlaylists = this.getUserPlaylists.bind(this);
-    this.setPlayingTrack = this.setPlayingTrack.bind(this);
   }
 
   componentDidMount() {
     this.getUserPlaylists();
-    Spotify.getAccessToken().then((token) => {
-      this.setState({ accessToken: token });
-    });
   }
 
   getUserPlaylists() {
@@ -43,15 +35,16 @@ class App extends React.Component {
   }
 
   addTrack(track) {
-    const tracks = this.state.playlistTracks;
+    let tracks = this.state.playlistTracks;
     if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
       return;
     }
-    this.setState({ playlistTracks: [...tracks, track] });
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
   removeTrack(track) {
-    const tracks = this.state.playlistTracks.filter(
+    let tracks = this.state.playlistTracks.filter(
       (currentTrack) => track.id !== currentTrack.id
     );
     this.setState({ playlistTracks: tracks });
@@ -62,70 +55,50 @@ class App extends React.Component {
   }
 
   savePlaylist() {
-    const trackUris = this.state.playlistTracks.map((track) => track.uri);
-    if (!trackUris.length) {
-      alert("Your playlist is empty! Please add tracks.");
-      return;
-    }
-
-    Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
-      this.getUserPlaylists();
-      this.setState({
-        playlistName: "New Playlist Name",
-        playlistTracks: [],
+    let trackUris = this.state.playlistTracks.map((track) => track.uri);
+    if (trackUris && trackUris.length) {
+      Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
+        this.getUserPlaylists();
+        this.setState({
+          playlistName: "New Playlist Name",
+          playlistTracks: [],
+        });
       });
-    });
+    } else {
+      alert("Your playlist is empty! Please add tracks.");
+    }
   }
 
   search(term) {
     Spotify.search(term).then((searchResults) => {
-      this.setState({ searchResults });
+      this.setState({ searchResults: searchResults });
     });
   }
 
-  setPlayingTrack(uri) {
-    this.setState({ currentlyPlayingUri: uri });
-  }
-
   render() {
-    const {
-      accessToken,
-      searchResults,
-      playlistName,
-      playlistTracks,
-      playlists,
-      currentlyPlayingUri,
-    } = this.state;
-
     return (
       <div>
         <h1>
-          <span className="highlight">MixMuse</span>
+          Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
           <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
-              searchResults={searchResults}
+              searchResults={this.state.searchResults}
+              onSearch={this.search}
               onAdd={this.addTrack}
-              onPlay={this.setPlayingTrack} // ⬅️ pass play handler
             />
             <Playlist
-              playlistName={playlistName}
-              playlistTracks={playlistTracks}
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
               onSave={this.savePlaylist}
             />
+            
           </div>
-          <PlaylistList playlists={playlists} />
-          {/* Render player if token and track are available */}
-          {accessToken && currentlyPlayingUri && (
-            <SpotifyPlayer
-              accessToken={accessToken}
-              trackUri={currentlyPlayingUri}
-            />
-          )}
+          <PlaylistList playlists={this.state.playlists} />
         </div>
       </div>
     );
