071bfb384f209fbde367bf0e201f0011466cc86d
VladyslavaDanylina <vladyslavadanylina@gmail.com>
2025-05-10 18:16:44 +0300

    player function

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Track.css";
 
+let globalAudio = new Audio();
+let currentPlayingTrackId = null;
+
 class Track extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPlaying: false,
+    };
+
+    this.audio = null;
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.togglePreview = this.togglePreview.bind(this);
+    this.handleAudioEnd = this.handleAudioEnd.bind(this);
+  }
+
+  componentWillUnmount() {
+    if (this.state.isPlaying) {
+      globalAudio.pause();
+      currentPlayingTrackId = null;
+    }
   }
 
   addTrack() {
@@ -14,6 +31,35 @@ class Track extends React.Component {
 
   removeTrack() {
     this.props.onRemove(this.props.track);
+  }
+
+  togglePreview() {
+    const { previewUrl } = this.props.track;
+
+    if (!previewUrl) return;
+
+    // Stop other track if this one isn't the current
+    if (currentPlayingTrackId !== this.props.track.id) {
+      globalAudio.src = previewUrl;
+      globalAudio.play();
+      globalAudio.onended = this.handleAudioEnd;
+
+      currentPlayingTrackId = this.props.track.id;
+      this.setState({ isPlaying: true });
+    } else {
+      if (this.state.isPlaying) {
+        globalAudio.pause();
+        this.setState({ isPlaying: false });
+      } else {
+        globalAudio.play();
+        this.setState({ isPlaying: true });
+      }
+    }
+  }
+
+  handleAudioEnd() {
+    this.setState({ isPlaying: false });
+    currentPlayingTrackId = null;
   }
 
   renderAction() {
@@ -25,29 +71,28 @@ class Track extends React.Component {
   }
 
   render() {
-    const { name, artist, album, albumCover, previewUrl } = this.props.track;
+    const { name, artist, album, albumCover } = this.props.track;
+    const { previewUrl } = this.props.track;
 
     return (
       <div className="Track">
         <div className="Track-left">
-        {albumCover && (
-          <img
-            src={albumCover}
-            alt={`${name} album cover`}
-            className="Track-album-art"
-            style={{ width: '64px', height: '64px', marginRight: '10px' }}
-          />
-        )}
-        <div className="Track-information">
-          <h3>{name}</h3>
-          <p>{artist} | {album}</p>
-          {previewUrl && (
-            <audio controls className="Track-audio">
-              <source src={previewUrl} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
+          {albumCover && (
+            <img
+              src={albumCover}
+              alt={`${name} album cover`}
+              className="Track-album-art"
+            />
           )}
-        </div>
+          <div className="Track-information">
+            <h3>{name}</h3>
+            <p>{artist} | {album}</p>
+            {previewUrl && (
+              <button className="Track-play-button" onClick={this.togglePreview}>
+                {this.state.isPlaying ? "⏸" : "▶️"}
+              </button>
+            )}
+          </div>
         </div>
         {this.renderAction()}
       </div>
@@ -56,4 +101,3 @@ class Track extends React.Component {
 }
 
 export default Track;
-
